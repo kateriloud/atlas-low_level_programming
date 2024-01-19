9deb48443942f5753cce4933cf0ecc935b1d5e8b
kateriloud <kateri.louderback@atlasschool.com>
2024-01-19 23:49:57 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main ()
-{
-puts("/"Programming is like building a multilingual puzzle");
-return 0;
-}
+/**
+ * main - puts
+ *
+ * Return: Always 0 (Success)
+ * */
+ int main (void)
+ {	 
+	 puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
+ }
