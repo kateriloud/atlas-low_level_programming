7889cfc96dc071619051d1c47d0841dba98b6b19
kateriloud <kateri.louderback@atlasschool.com>
2024-01-22 00:38:26 +0000

    lastdigit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	{
 	printf("Last difit of %d is %d and is 0\n", n, lastd);
 	}
-	else (lastd < 6 && lastd != 0);
+	else (lastd < 6 & lastd ! = 0);
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
