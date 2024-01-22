b54a1b8d52d826300e0764635f618864f2e08de5
kateriloud <kateri.louderback@atlasschool.com>
2024-01-22 22:20:43 +0000

    eqno

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * main - Prints all lowercsae letters except q and e
+ *
+ * Return: (0)
+ *
+ **/
+int main(void)
+{
+	char i = 'a';
+	
+	while (i <= 'z');
+	{
+		if (i != 'e' && != 'q' )
+		{
+		putchar(i);
+		i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
