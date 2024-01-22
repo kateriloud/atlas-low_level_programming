138ac69861715984be4ae9583089941b545fd258
kateriloud <kateri.louderback@atlasschool.com>
2024-01-22 19:58:40 +0000

    a-z

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - This prints all lowercase letters
+ *
+ *Return: always (0)
+ *
+ *
+ **/
+int main (void)
+{
+	int ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
