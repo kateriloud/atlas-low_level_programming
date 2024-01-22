0149bb06197f7ae0b0c9f696be6b0d3d4edc92e5
kateriloud <kateri.louderback@atlasschool.com>
2024-01-22 00:08:07 +0000

    posnegzero

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+/**
+ * main - Displays whether random number is pos,neg, or zero
+ *
+ * Return: 0 (always success)
+ */
+int main(void)
+{
 
-int main (){
-
-	int n = rand();
-
-	if (n > 0) {
-		printf("%d is positive\n", n);
-	} else if (n < 0) {
-		printf("%d is negative\n", n);
-	} else {
-		printf("%d is zero\n", n);
+	int n;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", (n));
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", (n));
+	}
+	else
+	{
+		printf("%d is zero\n", (n));
 	}
 
 	return 0;
