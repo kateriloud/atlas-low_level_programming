b97cf47578f7b6f10a77ec55596bebaa0131813a
kateriloud <kateri.louderback@atlasschool.com>
2024-01-22 00:10:24 +0000

    pos.neg.zero

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,7 @@ int main(void)
 {
 
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -26,5 +26,5 @@ int main(void)
 		printf("%d is zero\n", (n));
 	}
 
-	return 0;
+	return (0);
 }
