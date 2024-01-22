ab2cf7e1496c11ffba50c2cf76ca76c9a911b73b
kateriloud <kateri.louderback@atlasschool.com>
2024-01-22 23:34:25 +0000

    noeq

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,17 @@
  */
 int main(void)
 {
-	char i = 'a';
-	while (i <= 'z')
+char i = 'a';
+
+while (i <= 'z')
 	{
-		if (i != 'e' && 'q')
-		i++;
-		
+	if (i != 'e' && 'q')
+	{
 		putchar(i);
-		
 	}
+	i++;
+}
+
 	putchar('\n');
 	return (0);
 }
