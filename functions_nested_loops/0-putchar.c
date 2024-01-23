9d8a02f78548b6fe48b51011eaf05299ca579db3
kateriloud <kateri.louderback@atlasschool.com>
2024-01-23 03:37:34 +0000

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
+/**
  * main - Entry point
  *
  *
  * Description: Print putchar with new line
  *@parameter: printf putchar
  * Return: (0)
- */
+ **/
 int main(void)
 
 {
