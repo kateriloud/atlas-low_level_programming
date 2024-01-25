e84dc718328b9b9f352ca508979ab8688b9b7984
kateriloud <kateri.louderback@atlasschool.com>
2024-01-25 01:16:24 +0000

    numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -7,15 +7,12 @@
  */
 int main(void)
 {
-	int n;
+	int n = 0;
 
-	for (n = 0; n > 10; n++)
+	while ( n < 10)
 	{
-		putchar(n + 0);
+		printf("%d\n", n);
+		n++;
 	}
-
-	putchar('\n');
-
-return (0);
-
+	return (0);
 }
