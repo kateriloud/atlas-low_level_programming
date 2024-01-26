6d5022e4daa3656a815dd65df294e4c91327d723
kateriloud <kateri.louderback@atlasschool.com>
2024-01-26 21:48:50 +0000

    single

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all possible cominations of single digit numbers
+ * Return: (0)
+ */
+
+int main(void)
+
+{
+	num = 0;
+		while (num = 0; num <= 9; num++)
+		{
+			putchar(" ");
+			putchar(",");
+		}
+	done
+}
