6c246c3268078095f30b256c5fc56152737a5b28
kateriloud <kateri.louderback@atlasschool.com>
2024-01-26 21:06:12 +0000

    reverse

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
+/**
  * main - Entry point
- *
  * Description: prints lowercase alphabet  in reverse
  *
  * Return: (0)
- *
  */
 int main(void)
 {
