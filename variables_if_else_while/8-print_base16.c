9fc9695c068af52fe6219dabc820b233335335bb
kateriloud <kateri.louderback@atlasschool.com>
2024-01-26 21:24:55 +0000

    hexa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all the numbers of base of 16 in lowercase
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 15; num++)
+	{
+	
+		printf("%d", num);
+	
+	}
+
+	printf("\n");
+	return (0);
+
+}
