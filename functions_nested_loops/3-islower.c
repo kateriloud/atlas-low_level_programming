5e9d4b78de45feb11587fbcc98151808ce7d9508
kateriloud <kateri.louderback@atlasschool.com>
2024-01-26 23:55:00 +0000

    islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <main.h>
 
 /**
  *
  * main - function that checks for lowercase characters
- *
+ * @c - character to check
+ * 
  * Return: (0)
  * */
 
-int main(void)
+int islower(int c)
 {
+	
+	if (c >= 'a'; && c <= 'z')
+	{
+		return 1;
+	}
+		else
+		{
+			return 0;
+		}
 
 
-
-
-
-
-
+return (0);
 }
