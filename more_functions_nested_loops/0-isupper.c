a202bb1d9118b76d00a9ab9b0e20686caa0dee74
kateriloud <kateri.louderback@atlasschool.com>
2024-01-27 06:21:11 +0000

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: uppercase C
+ *
+ * Return: (0)
+ *
+ **/
+
+int _isupper(int c)
+{
+	char c;
+
+	c= 'C';
+	printf("%c: %d\n", _isupper(c));
+	c = 'c';
+	printf("%c: %d", c, _isupper(c));
+
+	return (0);
+}
