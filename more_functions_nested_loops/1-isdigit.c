0094e3e23e7c7469f75f4fabfb7a3fa296671662
kateriloud <kateri.louderback@atlasschool.com>
2024-01-27 07:55:03 +0000

    isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _isdigit - checks for digit 0 to 9
+ * @c: is digit
+ *
+ * Return: (0)
+ * */
+
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 57)
+		{
+		return (1);
+		}
+
+	return (0);
+
+}
