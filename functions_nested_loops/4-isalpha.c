37308ea8a2ae3adad8f59c84acc45f430c7cceb1
kateriloud <kateri.louderback@atlasschool.com>
2024-01-27 00:45:00 +0000

    alpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -13,10 +13,10 @@
 int _isalpha(int c)
 {
 
-	if (c >= 'a' && c <= 'z') && (c >= 'A' && c <= 'Z')
-	{
-	return (1);
-	}
+	if (c >= 'a' 'A' && c <= 'z' 'Z')
+		{
+		return (1);
+		}
 
 return (0);
 }
