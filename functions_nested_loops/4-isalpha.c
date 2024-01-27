9cd6165b65f6da527bf09df9c0bf795ca3ecbf29
kateriloud <kateri.louderback@atlasschool.com>
2024-01-27 00:53:43 +0000

    alpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 
 /**
  *
- * _isalpha - checks for alphabetic character 
+ * _isalpha - checks for alphabetic character
  * @c: lowercase or uppercase
  *
  * Return: (0)
@@ -13,8 +13,8 @@
 int _isalpha(int c)
 {
 
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c<= 'Z'))
-		
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+
 		{
 		return (1);
 		}
