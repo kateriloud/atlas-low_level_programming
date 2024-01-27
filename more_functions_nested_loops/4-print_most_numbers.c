f829232e45599efacfca0389a60b15c464ccc126
kateriloud <kateri.louderback@atlasschool.com>
2024-01-27 18:53:51 +0000

    not2or4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_most_numbers - function that prints numbers followed by a new line
+ * description - prints numbers 0 to 9 except  2 and 4
+ *
+ * return (0)
+ *
+ */
+void print_most_numbers(void)
+{
+
+	int k;
+	for (k = 48; k < 58; k++)
+	{
+		if (k == 50 && k == 52)
+			{
+			continue;
+			}
+		else
+			{
+			putchar(k);
+			}
+	}
+
+	putchar('\n');
+}
