ee7f6f38f2ae8699030dc30d570868171e004bec
kateriloud <kateri.louderback@atlasschool.com>
2024-01-27 00:36:47 +0000

    alpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * _isalpha - checks for alphabetic character 
+ * @c: lowercase or uppercase
+ *
+ * Return: (0)
+ *
+ **/
+
+int _isalpha(int c)
+{
+
+	if (c >= 'a' && c <= 'z' && c >= 'A' && c =< 'Z')
+	{
+	return (1);
+	}
+
+return (0);
+
+
+}
