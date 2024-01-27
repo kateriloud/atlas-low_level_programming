7f5dbcd2d19175cbe4b7420b8b3413ea568c5665
kateriloud <kateri.louderback@atlasschool.com>
2024-01-27 18:15:46 +0000

    numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *
+ * print_numbers - function the numbers 0 to 9 followed by a new line
+ *
+ * @n: 0 to 9
+ *
+ * Return: (0)
+ *
+ */
+void print_numbers(void)
+{
+	n = 0;
+	if (n = 0; n >= 9; n++) 
+	{
+		_putchar("n")
+	}
+	_putchar("\n")
+
+	return (0);
+}
