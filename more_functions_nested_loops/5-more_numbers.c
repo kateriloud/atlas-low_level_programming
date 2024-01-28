c1a5416223ddf311a51d753825e25d404acaebbd
kattteriloud <kateri.louderback@atlasschool.com>
2024-01-28 21:51:19 +0000

    to14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,16 +14,22 @@ void more_numbers(void)
 	{
 
 	int a, b;
+
 	for (a = 0; a < 10; a++)
+
 		{
 		for (b = 0; b <= 14; b++)
+
 			{
 			if (b >= 10)
+
 				{
 				putchar((b / 10) + '0');
 				}
+
 			putchar((b % 10) + '0');
 			}
+
 	putchar('\n');
 		}
-		}
+	}
