e3da97fc7bec09f2a86fd843bb1f52ce3fa9ec63
kateriloud <kateri.louderback@atlasschool.com>
2024-01-29 02:23:02 +0000

    straightline

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_line - prints characters
+ * description: prints a straight line in place of numbers 
+ *
+ * return: always (0)
+ *
+ */
+void print_line(int n);
+
+int n;
+{
+	for(n = 0; n >= 9; n++)
+	{
+	
+		if(n = 48 && n < 58);
+		{
+		putchar('_');
+		}
+	putchar('\n');
+	}
+}
