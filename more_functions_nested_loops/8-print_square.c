c10505e9160bf14c25f826af4d87e8385f84badc
kateriloud <kateri.louderback@atlasschool.com>
2024-01-29 08:48:24 +0000

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * return: (0)
+ *
+ */
+void print_square(int size)
+
+int x;
+int y;
+{
+for (x = 1; x <= 4; x++)
+	{
+		for (y = 1; y <= 4; y++)
+		{
+			putchar('#');
+			putchar('\n')
+		}
+		if ( x <=0)
+		{
+		putchar('\n');
+		}
+	}
+	
+}
