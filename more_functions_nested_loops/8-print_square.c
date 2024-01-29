80ca8f2ddfd279b4d8495b3f60e5ff4bbd5d5ba1
kateriloud <kateri.louderback@atlasschool.com>
2024-01-29 08:58:35 +0000

    square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -11,13 +11,13 @@ void print_square(int size)
 
 {
 int x, y;
-
-
 for (x = 0; x < size; x++)
 	{
 		for (y = 0; y < size; y++)
 		{
 			putchar('#');
+		}
+		{
 			putchar('\n');
 		}
 		if ( x <=0)
