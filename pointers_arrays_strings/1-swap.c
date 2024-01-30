2fcaaf91cf23f6376c199024624dfe0ca87b220a
kateriloud <kateri.louderback@atlasschool.com>
2024-01-30 04:50:15 +0000

    abswap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - swaps values of two integers
+ * @a: integer
+ * @b: integer
+ * return: (0)
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int x = *a;
+	*a = *b;
+	*b = x
+}
