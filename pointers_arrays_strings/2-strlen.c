40409d1bd2378f56c7d993e33ff3f1f82dde2a43
kateriloud <kateri.louderback@atlasschool.com>
2024-01-30 05:33:07 +0000

    strrrring

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - returns length of a string
+ * @s - string integer
+ * return: (0)
+ *
+ */
+int _strlen(char *s)
+{
+int k;
+
+k = '0';
+
+	while ([k] != \0)
+	{
+		k++;
+	}
+return (0);
+}
