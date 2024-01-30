4523265d46f30cecbc21ea639a902b04c51a9851
kateriloud <kateri.louderback@atlasschool.com>
2024-01-30 03:55:26 +0000

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 /**
- * print numbers 1 to 100
- * description: multiples of 3 print Fizz, multiples of 5 print Buzz, for multiples of both 3 and 5 print word FizzBuzz, print number otherwise and each print followed by space
+ * main - fizzbuzz number test
+ *
  * return: (0)
  *
- */
+ **/
+int main(void)
 {
 int x
 
@@ -33,4 +34,5 @@ for (x = 1; x <= 100; x++)
 		putchar(" ");
 		}
 	}
+return (0);
 }
