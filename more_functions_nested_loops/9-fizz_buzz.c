a62c82ff14d1f7832fb318804d68fdf86973ce76
kateriloud <kateri.louderback@atlasschool.com>
2024-01-30 03:45:05 +0000

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,10 +8,8 @@
  *
  */
 int x;
-
-{
 for(x = 1; x <= 100; x++)
-	{
+{
 	if((x % )3 == 0 && (x % 5) == 0)
 		{
 		putchar("FizzBuzz");
@@ -32,5 +30,5 @@ for(x = 1; x <= 100; x++)
 		putchar(x);
 		putchar(" ");
 		}
-	}
 }
+
