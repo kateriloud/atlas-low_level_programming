5e9a0fd20b4c5afddfbaef3495718f739dd7a5ad
kateriloud <kateri.louderback@atlasschool.com>
2024-02-02 04:47:03 +0000

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -16,17 +16,17 @@ int print_sign(int n)
 	if (n < 0)
 	{
 		value = -1;
-		putchar('-');
+		_putchar('-');
 	}
 	else if (n > 0)
 	{
 		value = 1;
-		putchar('+');
+		_putchar('+');
 	}
 	else
 	{
 		value = 0;
-		putchar('0');
+		_putchar('0');
 	}
 return (value);
 }
