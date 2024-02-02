834568766285deedcdf292fff4a8900a3fb32c3f
kateriloud <kateri.louderback@atlasschool.com>
2024-02-02 19:50:03 +0000

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int x;
 
-	for (i = 0; i < n && src[x] != '\0'; x++)
+	for (x = 0; x < n && src[x] != '\0'; x++)
 		dest[x] =  src[x];
 	for ( ; x < n; x++)
 		dest[x] = '\0';
