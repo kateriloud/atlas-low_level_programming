954831c7b0752533c5887758dd62f654b851a1b6
kateriloud <kateri.louderback@atlasschool.com>
2024-02-02 19:46:55 +0000

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: integer
+ *
+ * return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	for (i = 0; i < n && src[x] != '\0'; x++)
+		dest[x] =  src[x];
+	for ( ; x < n; x++)
+		dest[x] = '\0';
+
+	return (dest);
+}
