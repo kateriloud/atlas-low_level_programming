800cca80cb3411a94c91d13b89c48731c8fc50bf
kateriloud <kateri.louderback@atlasschool.com>
2024-02-02 08:41:50 +0000

    lastd

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit(int) - prints last digit of number
+ * return: last digit
+ *
+ **/
+int print_last_digit(int)
+{
+int last;
+
+	last = n % 10;
+	if (last < 0)
+	{
+	last = last * -1;
+	}
+		putchar(last);
+		return(last);
+}
