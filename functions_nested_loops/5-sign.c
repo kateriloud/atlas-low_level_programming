643b047b375a43b851e182fa041a3491bc855968
kateriloud <kateri.louderback@atlasschool.com>
2024-02-02 18:17:42 +0000

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_sign(int n) - prints positive, negative, or zero
+ * print_sign(int n)- prints positive, negative, or zero
  * @n: number
  * description: prints +1, -1, or 0 depending on the number
  * Return: (-1, 0, 1)
