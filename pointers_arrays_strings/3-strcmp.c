aeca6cb4612a1058b9d42ae8fc0b781eeb15c43a
kateriloud <kateri.louderback@atlasschool.com>
2024-02-02 21:34:32 +0000

    strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * return: 0
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+ 
+
+}
