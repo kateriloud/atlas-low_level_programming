e27423eb76b1e69f79b8eadadb41e795be07a9b4
kateriloud <kateri.louderback@atlasschool.com>
2024-02-03 04:07:06 +0000

    24

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /*
- * jack_bauer - function that prints every minute of the day
+ * jack_bauer - function that prints every minute of every hour of the day     
  *
  * return: 0
  */
