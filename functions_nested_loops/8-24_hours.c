baddf571db21fe9177af7d5d219ba6a7ee96d1cf
kateriloud <kateri.louderback@atlasschool.com>
2024-02-03 03:28:21 +0000

    24

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,22 +9,22 @@ void jack_bauer(void)
 {
 int x;
 int y;
-
-{
+	{
 
 	for (x = 0; x < 23; x++)
-	{
+		{
 		putchar(x / 10 + '0');
 		putchar(x % + '0');
 		putchar(':');
 		putchar('\n');
-	}
+		}
 	for (y = 0; y < 59; y++)
-	{
+		{
 		putchar(y / 10 + '0');
 		putchar(y % 10 + '0');
 		putchar(':');
 		putchar('\n');
+		}
 	}
-return: (void);
 }
+
