29ee2915cef6fc363f5992e0e61bdbff098df06b
kateriloud <kateri.louderback@atlasschool.com>
2024-02-03 03:18:57 +0000

    jack

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * jack_bauer - function that prints every minute of the day
+ *
+ * return: 0
+ */
+void jack_bauer(void)
+{
+int x;
+int y;
+
+{
+
+	for (x = 0; x < 23; x++)
+	{
+		putchar(x / 10 + '0');
+		putchar(x % + '0');
+		putchar(':');
+		putchar('\n');
+	}
+	for (y = 0; y < 59; y++)
+	{
+		putchar(y / 10 + '0');
+		putchar(y % 10 + '0');
+		putchar(':');
+		putchar('\n');
+	}
+}
