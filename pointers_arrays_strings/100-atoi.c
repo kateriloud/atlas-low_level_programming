2ca448f3f4b4c5ac746f7a93ec197abb1e91a3ff
kateriloud <kateri.louderback@atlasschool.com>
2024-02-03 20:27:57 +0000

    atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ *_atoi - function that converts a string to integer
+ * @s: string character
+ * return: integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+		if (s[0]  == '-')
+		{
+			sign = -1;
+			i++;
+		}
+
+		if (s[0] == '+')
+		{
+			i++;
+		}
+
+		while (s[i] != '\0')
+		{
+			if (s[i] >= '0' && <= '9')
+			{
+				result = result * 10 (s[i] - '0');
+			}
+			else
+			{
+			break;
+			}
+			i++;
+		}
+	result = result * sign;
+
+	return result;
+}
