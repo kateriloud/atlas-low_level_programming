f36b5395131afd8885cdf86f8b5ecb39aa7c0b53
kateriloud <kateri.louderback@atlasschool.com>
2024-02-03 22:41:57 +0000

    compare

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,12 +2,11 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strcmp - function that compares two strings
+  _strcmp - function that compares two strings
  * @s1: string 1
  * @s2: string 2
  *
  * return: 0
- *
  */
 int _strcmp(char *s1, char *s2)
 {
