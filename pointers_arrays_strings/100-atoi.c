3d708b5bbc86cfb3843e43d729b7435365d12ff8
kateriloud <kateri.louderback@atlasschool.com>
2024-02-03 20:32:39 +0000

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -23,9 +23,9 @@ int _atoi(char *s)
 
 		while (s[i] != '\0')
 		{
-			if (s[i] >= '0' && <= '9')
+			if (s[i] >= '0' && s[i] <= '9')
 			{
-				result = result * 10 (s[i] - '0');
+				result = i * 10 + (s[i] - '0');
 			}
 			else
 			{
