ab2cfa4cb915d9ceec56726b467b0333851b0c84
kateriloud <kateri.louderback@atlasschool.com>
2024-02-03 01:51:47 +0000

    lastd

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,11 @@ int print_last_digit(int n)
 {
 int last;
 
-	last = n % 10;
-	if (last < 0)
+	n %= 10;
+	if (n < 0)
 	{
-	last = last * -1;
+	n *= -1;
 	}
-		putchar(last);
+		putchar(n);
 		return(last);
 }
