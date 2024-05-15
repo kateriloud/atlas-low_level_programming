476727d44a6529687f2718eca586e19a57eb436a
kateri <kateriloud@gmail.com>
2024-05-15 23:42:24 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ * 
+ * description: this program prints a string using puts function
+ *
+ * @void: no parameters
+ *
+ * return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
