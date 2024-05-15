646cb5d1cec20721c37f141b697d0d1c017911b1
kateri <kateriloud@gmail.com>
2024-05-15 23:50:14 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,10 +6,10 @@
  *
  * @void: no parameters
  *
- * return: 0 (always success)
+ * return: (0 always success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
