0d897b5a02915d28f4578a84722571d73dfa4189
kateri <kateriloud@gmail.com>
2024-05-15 23:45:35 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - void
  *
  * description: this program prints a string using puts function
  *
@@ -8,7 +8,7 @@
  *
  * return: 0 always success
  */
-int main(0)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
