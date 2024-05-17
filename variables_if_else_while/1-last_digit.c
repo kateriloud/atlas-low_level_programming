7d72d2f149773428f65525270529d480ac4b05dd
kateri <kateriloud@gmail.com>
2024-05-17 17:49:29 +0000

    lastdigit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,20 +20,20 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n%10 > 5)
+	if (n %10 > 5)
 	{
 		printf("%s", x);
-		printf("%d is %d and is greater than 5\n", n, n%10);
+		printf("%d is %d and is greater than 5\n", n, n %10);
 	}
-	else if (n%10 < 6 && n%10 != 0)
+	else if (n %10 < 6 && n %10 != 0)
 	{
 		printf("%s", x);
-		printf("%d is %d and is less than 6 and not 0\n", n, n%10);
+		printf("%d is %d and is less than 6 and not 0\n", n, n %10);
 	}
 	else
 	{
 		printf("%s", x);
-		printf("%d is %d and is 0\n", n, n%10);
+		printf("%d is %d and is 0\n", n, n %10);
 	}
 	return (0);
 }
