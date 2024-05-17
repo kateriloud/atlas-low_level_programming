3ce114b7c26c34f53ad54a4354e1eb4202bf83be
kateri <kateriloud@gmail.com>
2024-05-17 05:10:01 +0000

    pos

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,15 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+
 /**
  * main - void
  *
- * Description: program assigns random num to variable n and states if pos or neg
+ * Description: prints random number and categorizes them by last digit
  *
  * @: no parameters
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
@@ -17,12 +19,8 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-		printf("%d is positive \n", n);
-	else if (n < 0)
-		printf("%d is negative \n", n);
-	else
-		printf("%d is zero \n", n);
+	if 
+
 
 	return (0);
 }
