d49db02b2109c66965b371fdeaabb3747f935514
kateri <kateriloud@gmail.com>
2024-05-17 22:20:53 +0000

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include <stdio.h>
 
 /**
  *
@@ -14,5 +14,14 @@
 
 int main(void)
 {
-
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
 }
