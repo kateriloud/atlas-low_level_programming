e63758664cee49ef8816edbb2ec40a58151982d2
kateri <kateriloud@gmail.com>
2024-05-17 21:31:28 +0000

    0-9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - void
+ *
+ * Description: prints all single digit numbers 0-9
+ *
+ * @: no parameters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = '0';
+
+	while (x <= 9)
+	{
+	putchar(x);
+	x++;
+	}
+	putchar('\n');	
+	return (0);
+}
