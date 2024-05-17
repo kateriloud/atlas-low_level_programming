cf723b7b11400598211ada3326480a72203ad09d
kateri <kateriloud@gmail.com>
2024-05-17 22:29:09 +0000

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  *
- * main - 
+ * main - void
  *
  * Description: program that prints _putchar with new line 
  *
@@ -14,14 +14,16 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
 	return (0);
 }
