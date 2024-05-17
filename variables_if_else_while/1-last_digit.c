6b3b95e48001979234b1e71199e71a8974b48af8
kateri <kateriloud@gmail.com>
2024-05-17 17:48:17 +0000

    lastdigit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 		printf("%s", x);
 		printf("%d is %d and is greater than 5\n", n, n%10);
 	}
-	else if (n%10 < 6 && !0)
+	else if (n%10 < 6 && n%10 != 0)
 	{
 		printf("%s", x);
 		printf("%d is %d and is less than 6 and not 0\n", n, n%10);
