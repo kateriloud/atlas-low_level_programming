800545ec7713cafdfa26df4ab26d4a8548e5986c
kateri <kateriloud@gmail.com>
2024-05-18 05:01:58 +0000

    islower

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include "ctype.h"
+
+/**
+ * main - entry point
+ * Description: program that searches and prints 1 for upper/lowercase
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	
+}
