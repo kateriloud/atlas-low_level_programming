0a819f73189704e2158415788ab8f3794a103283
kateri <kateriloud@gmail.com>
2024-05-18 01:52:03 +0000

    10times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * Main - void
+ *
+ * Description:
+ *
+ * @:
+ *
+ * Return: Always success (0)
+ */
+
+void print_alphabet_x10(void);
+{
+	for (int x = 0; x < 9; x++)
+	{
+		for (int y = 97; y < 122; y++)
+		_putchar(%c,y);
+	}
+		_putchar('\n');
+		return;
+}
