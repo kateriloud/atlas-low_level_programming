75402847217e5ee9d8fd278bbe61673b69a80d6e
kateri <kateriloud@gmail.com>
2024-05-18 04:38:24 +0000

    islower

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -14,13 +14,8 @@
 void print_alphabet_x10(void)
 
 {
-	for ( int x = 0; x < 9; x++ )
-	{
-		for ( int y = 97; y < 122; y++ )
-		{
-		_putchar(%c,y);
-		}
-	}
-		_putchar('\n');
-		return;
+	int x = 0;
+	
+	while
+
 }
---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include "ctype.h"
+/**
+ * main - entry point
+ * Description: program that checks for lowercase character
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if (islower(c) !=0)
+	{
+		_putchar('1');
+	}
+	return 0;
+}
