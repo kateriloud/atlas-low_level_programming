8c963ef56de72bd6f11545843144d372ad9ddc21
kateri <kateriloud@gmail.com>
2024-05-20 00:14:25 +0000

    islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -10,14 +10,14 @@
 int _islower(int c)
 {
 
-	if (c >= 'a' && c<= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 
-	return 0;
+	return;
 }
