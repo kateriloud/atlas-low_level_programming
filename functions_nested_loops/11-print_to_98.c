aa6e16fa35a48b663b2d7d8d11ee8a06df73c472
kateri <kateriloud@gmail.com>
2024-05-21 03:20:32 +0000

    98og

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,20 +12,18 @@
 
 void print_to_98(int n)
 {
-	if  (n <= 98)
-	{
-		while (n < 98)
-		{
-			printf("%d, ", n++);
-			printf("%d", n);
-		}
-	}
-	else if (n >= 98)
+	if  (n >= 98)
 	{
 		while (n > 98)
 		{
 			printf("%d, ", n--);
-			printf("%d", n);
+		}
+	}
+	if (n <= 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
 		}
 	}
 	return;
