25c0c7de2fca4e26cff43f7d9a58f2d28b59c804
kateri <kateriloud@gmail.com>
2024-05-21 04:16:42 +0000

    alphabt

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -14,12 +14,14 @@
 
 int main(void)
 {
-        char alpha = 'a';
-        while (alpha <= 'z' && alpha != 'e' && alpha != 'q')
-        {
-                putchar(alpha);
-                alpha++;
-        }
+        char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+			putchar(alpha);
+	}
+
         putchar('\n');
         return (0);
 } 
