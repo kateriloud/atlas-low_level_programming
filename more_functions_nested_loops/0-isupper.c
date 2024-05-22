1157dab204aaf7309a351fab6b52b112fccb0617
kateri <kateriloud@gmail.com>
2024-05-22 15:33:21 +0000

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include "main.h"
+#include <stdio.h>
+
+int _isupper(int c)
+{
+	char c;
+
+	if (c = 65; c <= 90; c++)
+	{
+		_putchar(c);
+		return (1);
+	}
+
+	if (c = 97; c <= 122; c++)
+	{
+		_putchar(c);
+		return(0);
+	}
+}
