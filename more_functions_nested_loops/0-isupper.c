7aa2914dc2961889a4b538cd3953b85f1417941a
kateri <kateriloud@gmail.com>
2024-05-22 16:09:09 +0000

    isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,6 +2,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _isupper - entry point
+ * Description: checks for uppercase letter & prints 1 and 0 for lowercase
+ * @c: int
+ * Return: Always 0 (success)
+ */
+
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
