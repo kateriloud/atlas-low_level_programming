9e5459f59400a7112b732fea95fa12c88ebab166
kateri <kateriloud@gmail.com>
2024-05-22 16:06:29 +0000

    isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,15 +4,15 @@
 
 int _isupper(int c)
 {
-	if (c >= 65 && c<= 90)
+	if (c >= 65 && c <= 90)
 	{
-		return(1);
+		return (1);
 	}
 
 	else if (c >= 97 && c <= 122)
 	{
-		return(0);
+		return (0);
 	}
 
-	return(0);
+	return (0);
 }
