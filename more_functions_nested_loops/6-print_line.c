c78506d0dc4c0f00cb60e5de98cdff32673dd624
kateri <kateriloud@gmail.com>
2024-05-22 23:56:28 +0000

    line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -4,4 +4,21 @@
 
 /**
  * print_line - entry point
- * Description: 
+ * Description: prints straight line of character using putchar 
+ * @n: int
+ * Return: Always 0 (success)
+ */
+
+void print_line(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('_');
+	}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	return;
+}
