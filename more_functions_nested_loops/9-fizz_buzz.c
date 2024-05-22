bd9ae66019c7596fa1dc7c2c5da6dd3a516b1ab5
kateri <kateriloud@gmail.com>
2024-05-22 22:05:56 +0000

    fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "main.h"
+
+
+/**
+ * main - entry point
+ * Description: fizz buzz print out
+ * @: no parameters
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for ( x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	printf("\n");
+	return (0);
+}
