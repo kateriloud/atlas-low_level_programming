7fdd376d25b1fee20f3a9b3dc850825f2f633f25
kateri <kateriloud@gmail.com>
2024-05-24 05:01:10 +0000

    stuff

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+#include <math.h>
+
+/**
+ * _strlen - entry point
+ * Description: function that counts string length
+ * @*s: char
+ * Return: Always 0 (success)
+ */
+
+int _strlen(char *s)
+{
+
+	return (length);
+}
