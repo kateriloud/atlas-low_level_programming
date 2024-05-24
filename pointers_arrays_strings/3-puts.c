3755d51e019d5b9e315dfbf73e179aa3dbaf4c67
kateri <kateriloud@gmail.com>
2024-05-24 17:22:00 +0000

    puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -4,4 +4,17 @@
 /**
  * _puts - entry point
  * Description: prints string without using puts
- * @
+ * @str: char
+ * Return: Always 0 (success)
+ */
+
+void _puts(char *str)
+{
+	while(*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+	return;
+}
