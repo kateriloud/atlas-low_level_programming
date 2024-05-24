a17c35462248a0dea13d46921656a62dad1e2266
kateri <kateriloud@gmail.com>
2024-05-24 04:14:43 +0000

    98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * reset_to_98 - entry point
+ * Description: takes a pointer to an int and updates value pointed to 98
+ * @*n: int type pointer
+ * Return: Always 0 (success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this project is introducing pointers and arrays to practice
